a751cb8a8b4b301f11903eb9bb02769e6c5b3922
Jaime <jaivalz@yahoo.com>
2015-04-07 17:15:25 +0000

    He modificado navigationlinkshtmlerb, contacthtmlerb, routes, contactscotroller, contactrb y contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Aprendo::Application.routes.draw do
- # get "/pages*id", to: 'pages#show'
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 end
 
 
 
+# para about y contact--------------------------------------------------
+#Aprendo::Application.routes.draw do
+  # get "/pages*id", to: 'pages#show' // es una explicación
+ # root to: 'visitors#new'
+#end
+#-------------------------------------------
 # Sin aplicar el high-voltage gem se ve asi
 #Aprendo::Application.routes.draw do
 #  root to: 'visitors#new'
