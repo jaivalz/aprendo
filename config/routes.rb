dc420e2a28d2f42ef6e7e8db94a0cd166cdf3a63
Jaime <jaivalz@yahoo.com>
2015-04-07 16:11:51 +0000

    He añadido la carpeta 'pages' y demás, tambien he modificado routesrb, contacthtmlerb, abouthtmlerb y navigationlinkshtmlerb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Aprendo::Application.routes.draw do
+ # get "/pages*id", to: 'pages#show'
   root to: 'visitors#new'
 end
+
+
+
+# Sin aplicar el high-voltage gem se ve asi
+#Aprendo::Application.routes.draw do
+#  root to: 'visitors#new'
+#end
