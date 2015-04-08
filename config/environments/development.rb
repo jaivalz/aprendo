dac22fa6a80796bb0b01966297c6b4e37b816e89
Jaime <jaivalz@yahoo.com>
2015-04-08 12:14:01 +0000

    Empezado el cap 23

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  
-    config.action_mailer.smtp_settings = {
+
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
@@ -36,9 +36,17 @@ Rails.application.configure do
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
-  
-  # Send email in development mode.
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
   config.action_mailer.perform_deliveries = true
+
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
